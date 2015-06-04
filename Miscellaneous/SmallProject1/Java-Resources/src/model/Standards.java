9c25d3a1f53b491e8f05366d82af22f5922589ff
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-06-04 08:35:34 -0400

    Create Standards.java

---
+++ Miscellaneous/SmallProject1/Java-Resources/src/model/Standards.java
@@ -0,0 +1,92 @@
+package model;
+
+public class Standards {
+
+	private String status;
+	private String chemical_id;
+	private String std_type;
+	private String std_value;
+	private String manufacturer_name;
+	private String lot;
+	private String expiration_date;
+	private String date_of_receipt;
+	private String date_of_first_use;
+	private String purity_grade;
+	private String date_of_preparation;
+	private String location;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getChemical_id() {
+		return chemical_id;
+	}
+	public void setChemical_id(String chemical_id) {
+		this.chemical_id = chemical_id;
+	}
+	public String getStd_type() {
+		return std_type;
+	}
+	public void setStd_type(String std_type) {
+		this.std_type = std_type;
+	}
+	public String getStd_value() {
+		return std_value;
+	}
+	public void setStd_value(String std_value) {
+		this.std_value = std_value;
+	}
+	public String getManufacturer_name() {
+		return manufacturer_name;
+	}
+	public void setManufacturer_name(String manufacturer_name) {
+		this.manufacturer_name = manufacturer_name;
+	}
+	public String getLot() {
+		return lot;
+	}
+	public void setLot(String lot) {
+		this.lot = lot;
+	}
+	public String getExpiration_date() {
+		return expiration_date;
+	}
+	public void setExpiration_date(String expiration_date) {
+		this.expiration_date = expiration_date;
+	}
+	public String getDate_of_receipt() {
+		return date_of_receipt;
+	}
+	public void setDate_of_receipt(String date_of_receipt) {
+		this.date_of_receipt = date_of_receipt;
+	}
+	public String getDate_of_first_use() {
+		return date_of_first_use;
+	}
+	public void setDate_of_first_use(String date_of_first_use) {
+		this.date_of_first_use = date_of_first_use;
+	}
+	public String getPurity_grade() {
+		return purity_grade;
+	}
+	public void setPurity_grade(String purity_grade) {
+		this.purity_grade = purity_grade;
+	}
+	public String getDate_of_preparation() {
+		return date_of_preparation;
+	}
+	public void setDate_of_preparation(String date_of_preparation) {
+		this.date_of_preparation = date_of_preparation;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+	
+}
